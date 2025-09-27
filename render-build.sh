2645a7b97f188d748861ad1fda195c637bb0b1bf
accold <chill_out95@live.com>
2025-09-27 15:51:14 -0700

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Install dependencies
+echo "Installing Node dependencies..."
 npm install
 
-echo "Build completed - Puppeteer 19.x will download Chromium automatically"
+# Puppeteer cache paths
+PUPPETEER_CACHE_DIR=$HOME/.cache/puppeteer
+XDG_CACHE_HOME=$HOME/.cache
+
+mkdir -p "$PUPPETEER_CACHE_DIR"
+
+# Copy Puppeteer from cache if exists
+if [[ -d "$XDG_CACHE_HOME/puppeteer" ]]; then
+    echo "...Copying Puppeteer cache from build cache"
+    cp -R "$XDG_CACHE_HOME/puppeteer/" "$PUPPETEER_CACHE_DIR"
+fi
+
+echo "...Puppeteer cache ready"
