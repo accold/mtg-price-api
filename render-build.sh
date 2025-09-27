54767868203ae29b1042c15f9f7c26360cb9e000
accold <chill_out95@live.com>
2025-09-27 15:56:42 -0700

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -10,10 +10,12 @@ XDG_CACHE_HOME=$HOME/.cache
 
 mkdir -p "$PUPPETEER_CACHE_DIR"
 
-# Copy Puppeteer from cache if exists
-if [[ -d "$XDG_CACHE_HOME/puppeteer" ]]; then
+# Only copy if source exists AND is different from destination
+if [[ -d "$XDG_CACHE_HOME/puppeteer" && "$XDG_CACHE_HOME/puppeteer" != "$PUPPETEER_CACHE_DIR" ]]; then
     echo "...Copying Puppeteer cache from build cache"
     cp -R "$XDG_CACHE_HOME/puppeteer/" "$PUPPETEER_CACHE_DIR"
+else
+    echo "...Puppeteer cache already exists or source same as destination"
 fi
 
 echo "...Puppeteer cache ready"
